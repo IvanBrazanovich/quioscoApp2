b238ae7da4b0be66d30b38bbe8589c15cd95f79d
Ivan <ivanbrazas@gmail.com>
2022-10-27 22:17:54 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>GuitarLA - Tienda de Guitarras</title>
+        <meta
+          name="description"
+          content="Tienda de Guitarras e información sobre música"
+        />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+}
